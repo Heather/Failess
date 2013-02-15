1c540d010b4cc8e7dfdc616a7fde759d1282768f
Heather Mason <heather@cynede.net>
2013-02-15 15:09:13 +0400

    pt

--- src/app/FailessLib/CSSValues.fs
+++ src/app/FailessLib/CSSValues.fs
@@ -12,5 +12,8 @@ let pxx vv = [for v in vv -> px v]
 let em (v : double) = v.ToString(nfi) +++ "em"
 let emm vv = [for v in vv -> em v]
 
+let pt v = s v +++ "pt"
+let ptt vv = [for v in vv -> pt v]
+
 let prc v = s v +++ "%"
 let prcc vv = [for v in vv -> prc v]
