6860902543cbff8c5098471ed4cae7d32acca762
ℋeatђer <heather@cynede.net>
2013-02-19 14:12:56 +0400

    Update examples/build.cmd

--- examples/build.cmd
+++ examples/build.cmd
@@ -6,16 +6,16 @@ set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 pushd .
 cd D:\...........\Failess\build\
 set ABS_PATH=%CD%
-if not exist "%ABS_PATH%\tools\FAKE\tools\Fake.exe" (
-	:: site.css
-	"Failess.exe" "D:\.......\Site.fsx"
+
+:: site.css
+"Failess.exe" "D:\.......\Site.fsx"
 	
-	:: PropertyGrid
-	"Failess.exe" "D:\.......\PropertyGrid.fsx"
+:: PropertyGrid
+"Failess.exe" "D:\.......\PropertyGrid.fsx"
 	
-	:: Settings.css
-	"Failess.exe" "D:\.........\Settings.fsx"
-	)
+:: Settings.css
+"Failess.exe" "D:\.........\Settings.fsx"
+
 popd
 
 pause
