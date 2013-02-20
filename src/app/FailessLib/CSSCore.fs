232440e4c09d2728705ee25c45dbda0c8fe8515b
Heather Mason <heather@cynede.net>
2013-02-20 16:29:30 +0400

    remove failess lib

--- src/app/FailessLib/CSSCore.fs
+++
@@ -1,9 +0,0 @@
-﻿[<AutoOpen>]
-module Failess.CSSCore
-open System
-open System.ComponentModel
-
-let inline (+++) a b = sprintf "%s%s" a b
-let inline (++) a b = sprintf "%s %s" a b
-
-let inline s str = str.ToString()
