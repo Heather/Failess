ede0a271be53deeb130aa76c6c2fcfca2bb60595
Heather <heather@live.ru>
2014-02-27 12:40:10 +0400

    tests: test list

--- src/app/FailTests/FailessTests.fs
+++ src/app/FailTests/FailessTests.fs
@@ -1,20 +1,42 @@
 ﻿open FailessTestData
 
+open System
+
 open Fuchu
 open Failess
 open FailessTestData
 open System.Text.RegularExpressions
 
-let convertTest = 
-    testCase "CSSConvert Test" <| 
-        fun _ -> 
-            let actual = toFailess <| ( Array.toList  // use unix end lines
-                                   <|   Regex.Split(css, "/n"))
-            "Actual : " + actual |> System.Console.WriteLine
-            Assert.Equal("ToFailess"
-                , failess
-                , actual
-                )
+let tests = 
+    testList "Failess tests" [ 
+        testCase "Failess Test" <| 
+            fun _ -> 
+                Assert.Equal("Failess"
+                    , ( try CSS ("custom.css") [
+                                code @ Color.black
+                                "/* When highlighted code blocks are too wide, they wrap. Resulting in the */"
+                                "/* line numbers column's rows not lining up with the code rows. Prevent */"
+                                "/* wrapping. */"
+                                pre -|[
+                                    whiteSpace  -- pre
+                                    width       -- inh
+                                ]
+                            ]
+                            "CSS: " + String.Join(System.Environment.NewLine
+                                      , System.IO.File.ReadAllLines("custom.css"))
+                            |> System.Console.WriteLine
+                            true
+                        with | _ -> false)
+                    , true)
+        testCase "CSSConvert Test" <| 
+            fun _ -> 
+                let actual = toFailess <| ( Array.toList
+                                       <|   Regex.Split(css, System.Environment.NewLine))
+                Assert.Equal("ToFailess"
+                    , failess
+                    , actual
+                    )
+        ]
 
 [<EntryPoint>]
-let main _ = run convertTest
+let main _ = run tests
